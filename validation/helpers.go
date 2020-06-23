a4be844cf43703027116ae0976b8196439174c45
Lewis Miller <l.w.miller@lancaster.ac.uk>
2020-06-23 01:14:43 +0100

    GoReportCard prettiness

--- validation/helpers.go
+++ validation/helpers.go
@@ -44,28 +44,28 @@ const (
 )
 
 const (
-	// InvalidUrlErrTemplateString: [Type Property] '[Value]' does not start with "http://" | "https://" | "attachment://"
+	// InvalidUrlErrTemplateString : [Type Property] '[Value]' does not start with "http://" | "https://" | "attachment://"
 	InvalidUrlErrTemplateString = `%v '%v' does not start with "http://" | "https://" | "attachment://"`
 
-	// InvalidHWErrTemplateString:  [Type] height '[Value]' or [Type] width '[Value]' is less than or equal to 0
+	// InvalidHWErrTemplateString :  [Type] height '[Value]' or [Type] width '[Value]' is less than or equal to 0
 	InvalidHWErrTemplateString = `%v height '%v' or %v width '%v' is less than or equal to 0`
 
-	// CharacterCountExceedsLimitErrTemplateString: [Type Property] exceeds [Limit]: length = [Length] | '[Value]'
+	// CharacterCountExceedsLimitErrTemplateString : [Type Property] exceeds [Limit]: length = [Length] | '[Value]'
 	CharacterCountExceedsLimitErrTemplateString = `%v exceeds %v characters: length = %v | '%v'`
 
-	// CharacterCountExceedsLimitLongErrTemplateString: [Type Property] exceeds [Limit]: length = [Length]
+	// CharacterCountExceedsLimitLongErrTemplateString : [Type Property] exceeds [Limit]: length = [Length]
 	CharacterCountExceedsLimitLongErrTemplateString = `%v exceeds %v characters: length = %v`
 
-	// FieldLimitReachedErrTemplateString: adding field '[FieldName]' would cause field count to exceed [Limit]
+	// FieldLimitReachedErrTemplateString : adding field '[FieldName]' would cause field count to exceed [Limit]
 	FieldLimitReachedErrTemplateString = `adding field '%v' would cause field count to exceed %v`
 
-	// InvalidEmbedTypeErrTemplateString: embed type '[Type]' is not one of "rich" | "image" | "video" | "gifv" | "link" | "article"
+	// InvalidEmbedTypeErrTemplateString : embed type '[Type]' is not one of "rich" | "image" | "video" | "gifv" | "link" | "article"
 	InvalidEmbedTypeErrTemplateString = `embed type '%v' is not one of "rich" | "image" | "video" | "gifv" | "link" | "article"`
 
-	// ValueNotBetweenErrTemplateString: [Type Property] [Value] is not between [LowerLimit] and [UpperLimit]
+	// ValueNotBetweenErrTemplateString : [Type Property] [Value] is not between [LowerLimit] and [UpperLimit]
 	ValueNotBetweenErrTemplateString = `%v '%v' is not between %v and %v`
 
-	// ValueIsEmptyErrString: [Type Property] should not be empty if set
+	// ValueIsEmptyErrString : [Type Property] should not be empty if set
 	ValueIsEmptyErrString = `%v should not be empty if set`
 )
 
